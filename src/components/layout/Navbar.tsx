95b34d6e343eab82087eb8bfa155c69bf5471f69
Abdiel Mendoza <abdielcomponentes.97@gmail.com>
2026-09-19 02:14:15 -0600

    navbar responsive con menu hamburguesa para movil

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,16 +1,35 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 function Navbar() {
+  const [menuAbierto, setMenuAbierto] = useState(false)
+
+  const cerrarMenu = () => setMenuAbierto(false)
+
   return (
     <nav className="navbar">
-      <div className="navbar-logo">PartyDog</div>
-      <ul className="navbar-links">
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/categorias">Categorías</Link></li>
-        <li><Link to="/productos">Productos</Link></li>
-        <li><Link to="/faq">FAQ</Link></li>
-        <li><Link to="/contacto">Contacto</Link></li>
+      <Link to="/" className="navbar-logo" onClick={cerrarMenu}>
+        PartyDog
+      </Link>
+
+      <button
+        className="navbar-toggle"
+        onClick={() => setMenuAbierto(!menuAbierto)}
+        aria-label="Abrir menú"
+        aria-expanded={menuAbierto}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <ul className={`navbar-links ${menuAbierto ? 'navbar-links-open' : ''}`}>
+        <li><Link to="/" onClick={cerrarMenu}>Inicio</Link></li>
+        <li><Link to="/categorias" onClick={cerrarMenu}>Categorías</Link></li>
+        <li><Link to="/productos" onClick={cerrarMenu}>Productos</Link></li>
+        <li><Link to="/faq" onClick={cerrarMenu}>FAQ</Link></li>
+        <li><Link to="/contacto" onClick={cerrarMenu}>Contacto</Link></li>
         <li>Carrito</li>
       </ul>
     </nav>
